1e77231b142adc1aabf595cf0e28429ad1c32ea4
Md. Saidur Rahman <37741326+saidurNsu99@users.noreply.github.com>
2024-02-22 16:55:42 +0600

    Update App.jsx
    fix for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>hello Recruiter you are inside my first react app , and i am going to rock your world</h1>
+      <h1>hello Recruiter you are inside my first react app,
+       and i am going to rock your world</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
